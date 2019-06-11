044e460b4432b327b34d960c23a699308dc73e8e
Primo <44984918+m-primo@users.noreply.github.com>
2019-06-11 06:46:35 +0200

    upload the script

---
+++ js/script.js
@@ -0,0 +1,34 @@
+var crazyClicker = {
+    counter: 0,
+    get Reset() {
+        this.counter = 0;
+    },
+    get Increment() {
+        this.counter++;
+    },
+    get Show() {
+        document.getElementById('counter').innerHTML = this.counter;
+    },
+    get Click() {
+        this.Increment;
+        this.Show;
+        if(this.counter % 100 == 0) {
+            if(this.counter != 0) {
+                alert('Congrats! You got ' + this.counter + ' clicks.');
+            }
+        }
+    },
+};
+
+window.onload = function() {
+    
+    document.getElementById('clickBTN').onclick = function() {
+        crazyClicker.Click;
+    }
+
+    document.getElementById('resetBTN').onclick = function() {
+        crazyClicker.Reset;
+        crazyClicker.Show;
+    }
+
+};
